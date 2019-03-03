007af56b323a1440e5a2fc87182e5c866c209220
Tim Liu <tim.liu@uwaterloo.ca>
2019-03-02 19:22:32 -0500

    copy instead of mv script

--- rebuild_all.sh
+++ rebuild_all.sh
@@ -1,3 +1,3 @@
 ./reinstall.sh
 ./tools/core_control/reinstall.sh
-mv ./tools/core_control/ccontrol.* /var/www/html/dji_naza_web_interface/bins
+cp ./tools/core_control/ccontrol.* /var/www/html/dji_naza_web_interface/bins
